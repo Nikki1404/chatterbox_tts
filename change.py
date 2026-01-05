91538b1e5c021a58f9fa90fc305b810600b6fa55
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-05 15:57:46 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,21 +1,41 @@
-9.635 Reading package lists...
-10.77 Building dependency tree...
-11.04 Reading state information...
-11.10 E: Unable to locate package python3.12-distutils
-11.10 E: Couldn't find any package by glob 'python3.12-distutils'
-11.10 E: Couldn't find any package by regex 'python3.12-distutils'
-------
-Dockerfile:13
---------------------
-  12 |
-  13 | >>> RUN add-apt-repository ppa:deadsnakes/ppa && \
-  14 | >>>     apt-get update && \
-  15 | >>>     apt-get install -y \
-  16 | >>>     python3.12 \
-  17 | >>>     python3.12-dev \
-  18 | >>>     python3.12-venv \
-  19 | >>>     python3.12-distutils && \
-  20 | >>>     rm -rf /var/lib/apt/lists/*
-  21 |
---------------------
-ERROR: failed to build: failed to solve: process "/bin/sh -c add-apt-repository ppa:deadsnakes/ppa &&     apt-get update &&     apt-get install -y     python3.12     python3.12-dev     python3.12-venv     python3.12-distutils &&     rm -rf /var/lib/apt/lists/*" did not complete successfully: exit code: 100
+FROM nvidia/cuda:12.1.0-runtime-ubuntu22.04
+
+
+RUN apt-get update && apt-get install -y \
+    software-properties-common \
+    ffmpeg \
+    libsndfile1 \
+    curl \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+
+RUN add-apt-repository ppa:deadsnakes/ppa && \
+    apt-get update && \
+    apt-get install -y \
+    python3.12 \
+    python3.12-dev \
+    python3.12-venv && \
+    rm -rf /var/lib/apt/lists/*
+
+
+RUN curl -sS https://bootstrap.pypa.io/get-pip.py | python3.12
+
+RUN ln -s /usr/bin/python3.12 /usr/bin/python && \
+    ln -s /usr/bin/python3.12 /usr/bin/python3
+
+ENV PYTHONUNBUFFERED=1
+ENV HF_HOME=/root/.cache/huggingface
+
+WORKDIR /app
+
+RUN pip install --upgrade pip setuptools wheel
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8003
+
+CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "8003", "--timeout-keep-alive", "600"]
