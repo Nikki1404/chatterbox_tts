77df7c89d5e0d91b36480b58efc648912b6d7537
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-05 15:39:34 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,1 +1,11 @@
-OUTPUT_CSV = Path(__file__).parent / "whisperx_benchmark_results.csv"
+[DONE] 5683-32879-0003.wav
+[DONE] 5683-32879-0022.wav
+[DONE] 5683-32879-0008.wav
+[DONE] 5683-32879-0018.wav
+[DONE] 5683-32879-0013.wav
+Traceback (most recent call last):
+  File "/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/utils/benchmark/whisperx_benchmark.py", line 173, in <module>
+    main()
+  File "/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/utils/benchmark/whisperx_benchmark.py", line 156, in main
+    with open(OUTPUT_CSV, "w", newline="", encoding="utf-8") as f:
+PermissionError: [Errno 13] Permission denied: '/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/utils/benchmark/whisperx_benchmark_results.csv'
