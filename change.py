84baa93696f1fc7f7c36744baebf031eaaa5ab4a
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-05 19:07:30 +0530

    Update change.py

--- change.py
+++ change.py
@@ -6,3 +6,6 @@ Please read the following numbers clearly: 42, 3.1416, and 1,000,000.
 The quick brown fox jumps over the lazy dog.
 Can you hear the difference between a question and a statement?
 Thank you for listening, and have a great day!
+
+Metrics: {'mode': 'sentence_chunked', 'clone_voice': True, 'sentences': 10, 'ttfa_sec': 4.8813, 'latency_sec_total': 34.4067, 'audio_sec_total': 37.08, 'rtf': 0.9279}
+Metrics: {'mode': 'sentence_chunked', 'clone_voice': False, 'sentences': 10, 'ttfa_sec': 0.996, 'latency_sec_total': 22.9457, 'audio_sec_total': 26.56, 'rtf': 0.8639}
