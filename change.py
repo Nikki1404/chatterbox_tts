687dbfb7041a13e98ab2bfb1ef861780f235714f
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-05 15:55:49 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,11 +1,21 @@
-[DONE] 5683-32879-0003.wav
-[DONE] 5683-32879-0022.wav
-[DONE] 5683-32879-0008.wav
-[DONE] 5683-32879-0018.wav
-[DONE] 5683-32879-0013.wav
-Traceback (most recent call last):
-  File "/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/utils/benchmark/whisperx_benchmark.py", line 173, in <module>
-    main()
-  File "/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/utils/benchmark/whisperx_benchmark.py", line 156, in main
-    with open(OUTPUT_CSV, "w", newline="", encoding="utf-8") as f:
-PermissionError: [Errno 13] Permission denied: '/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/utils/benchmark/whisperx_benchmark_results.csv'
+9.635 Reading package lists...
+10.77 Building dependency tree...
+11.04 Reading state information...
+11.10 E: Unable to locate package python3.12-distutils
+11.10 E: Couldn't find any package by glob 'python3.12-distutils'
+11.10 E: Couldn't find any package by regex 'python3.12-distutils'
+------
+Dockerfile:13
+--------------------
+  12 |
+  13 | >>> RUN add-apt-repository ppa:deadsnakes/ppa && \
+  14 | >>>     apt-get update && \
+  15 | >>>     apt-get install -y \
+  16 | >>>     python3.12 \
+  17 | >>>     python3.12-dev \
+  18 | >>>     python3.12-venv \
+  19 | >>>     python3.12-distutils && \
+  20 | >>>     rm -rf /var/lib/apt/lists/*
+  21 |
+--------------------
+ERROR: failed to build: failed to solve: process "/bin/sh -c add-apt-repository ppa:deadsnakes/ppa &&     apt-get update &&     apt-get install -y     python3.12     python3.12-dev     python3.12-venv     python3.12-distutils &&     rm -rf /var/lib/apt/lists/*" did not complete successfully: exit code: 100
