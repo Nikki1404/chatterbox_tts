e8952d93f310c37c8ebaa73c71eab6b03f16b323
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-06 17:20:53 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,4 +1,8 @@
-curl --location 'https://whisperstream.exlservice.com/speech-api/upload_file'
+ curl --location 'https://127.0.0.1:82/upload_file'
 --header 'Accept: /'
 --header 'asrPipeline: riva'
 --form 'file=@"/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/audio_samples/harvard.wav"'
+curl: (7) Failed to connect to 127.0.0.1 port 82 after 0 ms: Connection refused
+--header: command not found
+--header: command not found
+--form: command not found
