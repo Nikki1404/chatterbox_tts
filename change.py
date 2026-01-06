bfc3867084fb4ea7b9902f9871c26eecdf935a37
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-06 11:25:53 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,6 +1,3 @@
-Enter text (end with empty line, or 'exit'):
-Hello! This is a test of the text-to-speech system.
-
 Traceback (most recent call last):
   File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client.py", line 102, in <module>
     asyncio.run(main())
@@ -8,14 +5,12 @@ Traceback (most recent call last):
     return loop.run_until_complete(main)
   File "/usr/lib/python3.10/asyncio/base_events.py", line 649, in run_until_complete
     return future.result()
-  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client.py", line 60, in main
-    async with websockets.connect(
-  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/websockets/asyncio/client.py", line 587, in __aenter__
-    return await self
-  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/websockets/asyncio/client.py", line 541, in __await_impl__
-    self.connection = await self.create_connection()
-  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/websockets/asyncio/client.py", line 440, in create_connection
-    transport = await connect_http_proxy(
-  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/websockets/asyncio/client.py", line 815, in connect_http_proxy
-    await protocol.response
-websockets.exceptions.InvalidProxyStatus: proxy rejected connection: HTTP 400
+  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client.py", line 84, in main
+    sf.write(out, wav, sr)
+  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/soundfile.py", line 363, in write
+    with SoundFile(file, 'w', samplerate, channels,
+  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/soundfile.py", line 690, in __init__
+    self._file = self._open(file, mode_int, closefd)
+  File "/home/CORP/re_nikitav/cx-speech-voice-cloning/client/client_env/lib/python3.10/site-packages/soundfile.py", line 1265, in _open
+    raise LibsndfileError(err, prefix="Error opening {0!r}: ".format(self.name))
+soundfile.LibsndfileError: Error opening 'outputs/tts_2026-01-06_05-54-27_9d8fac.wav': System error.
