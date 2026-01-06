c37c310fb29e0ac113141bf7c9599620a689c08c
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-06 16:51:36 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,4 +1,4 @@
 curl --location 'https://whisperstream.exlservice.com/speech-api/upload_file'
 --header 'Accept: /'
 --header 'asrPipeline: riva'
---form 'file=@"a.wav"'
+--form 'file=@"/home/CORP/re_nikitav/bu-digital-cx-asr-whiperx/audio_samples/harvard.wav"'
