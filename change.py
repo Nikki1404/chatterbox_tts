29a1f19d424c36604b128f27aeb6e4120d9ac935
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-07 14:27:47 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,7 @@
-export no_proxy=10.0.0.0/8,localhost,127.0.0.1
-export NO_PROXY=10.0.0.0/8,localhost,127.0.0.1
-
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# export no_proxy=10.0.0.0/8,localhost,127.0.0.1
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# export NO_PROXY=10.0.0.0/8,localhost,127.0.0.1
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav#  wget http://10.90.126.61:9000/
+--2026-01-07 08:56:48--  http://10.90.126.61:9000/
+Connecting to 163.116.128.80:8080... connected.
+Proxy request sent, awaiting response... 400 Bad Request
+2026-01-07 08:56:48 ERROR 400: Bad Request.
