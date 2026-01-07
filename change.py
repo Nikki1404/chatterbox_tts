4edeb7b4e5c37c52a5d728e2c75a874b88c4243e
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-07 14:14:11 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,5 +1,5 @@
 aws ssm start-session \
-  --region ap-south-1 \
+  --region us-east-1 \
   --target i-05a31ee067e1506cb \
   --document-name AWS-StartPortForwardingSession \
   --parameters '{"portNumber":["22"],"localPortNumber":["2222"]}'
