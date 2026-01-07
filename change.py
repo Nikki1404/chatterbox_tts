2bf8a001e8314814bcf72c515f43e42980b98795
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-07 14:26:46 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,5 +1,3 @@
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# wget http://10.90.126.61:9000/
---2026-01-07 08:54:03--  http://10.90.126.61:9000/
-Connecting to 163.116.128.80:8080... connected.
-Proxy request sent, awaiting response... 400 Bad Request
-2026-01-07 08:54:03 ERROR 400: Bad Request.curl -s http://169.254.169.254/latest/meta-data/iam/info
+export no_proxy=10.0.0.0/8,localhost,127.0.0.1
+export NO_PROXY=10.0.0.0/8,localhost,127.0.0.1
+
