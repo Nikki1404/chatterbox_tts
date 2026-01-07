9738bd3c56e3afae45a0c1fe95bc9c5b8b489723
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-07 14:48:03 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,4 +1,3 @@
-curl -X POST "http://127.0.0.1:8002/asr/upload_file" \
-  -H "debug: yes" \
-  -H "diarization: true" \
-  -F "file=@sample.mp4"
+re_nikitav@EC03-E01-AICOE1:~/bu-digital-cx-asr-whiperx$ curl -X POST "http://127.0.0.1:8002/asr/upload_file"   -H "debug: yes"   -H "diarization: true"   -F "file=@inspira_audio/2025-08-01-14-39-52_679553708932_VOICE_84777115-7c03-4283-b56c-c565a9b5e6f9.mp4"
+
+
