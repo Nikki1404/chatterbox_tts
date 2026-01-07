dac58d01cd107046673af04965f4aae2c89066ed
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-07 13:56:43 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,1 +1,3 @@
-For Chatterbox, an 8-second reference audio is enough to pick up voice tone, but not sufficient for stable voice cloning or accent consistency
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# ss -tulpn | grep ssh
+tcp   LISTEN 0      128              0.0.0.0:22         0.0.0.0:*    users:(("sshd",pid=2234707,fd=3))                                                      
+tcp   LISTEN 0      128                 [::]:22            [::]:*    users:(("sshd",pid=2234707,fd=4))                  
